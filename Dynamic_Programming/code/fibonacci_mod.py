7622bf1bd45160a0fad42891e2a369607ceedc38
Alexander <Sanyarozl@gmail.com>
2018-11-27 22:54:26 +0200

    Add min_max_afford and tests

--- Dynamic_Programming/code/fibonacci_mod.py
+++ Dynamic_Programming/code/fibonacci_mod.py
@@ -1,8 +1,11 @@
-"""Implemente task 2."""
+"""Implementing task 2.
+Find which is the member of position n
+ in Modification Fibonacci sequence.
+"""
 
 
 def fibo(size):
-    """Calculate Modified Fibonacci."""
+    """Calculate Modified Fibonacci number."""
     if size in (1, 2, 3):
         return 1
     return fibo(size-1) + fibo(size-3)
