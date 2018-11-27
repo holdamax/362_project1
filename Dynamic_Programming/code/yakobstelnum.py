fb1f7cc6b2857c439f86844f54d15812f9900003
marianmagur <marianmagur@gmail.com>
2018-11-27 22:51:49 +0200

    add foldeer tests

--- Dynamic_Programming/code/yakobstelnum.py
+++ Dynamic_Programming/code/yakobstelnum.py
@@ -1,6 +1,6 @@
-""""count yacobstel number"""
+""""count yacobstel numbers"""
 def yakobstelelement (n_j):
 
     return(2**n_j-(-1)**n_j)/3
 
-print (yakobstelelement(4))
+print (yakobstelelement(6))
