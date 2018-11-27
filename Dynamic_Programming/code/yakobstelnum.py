9deb4c6369d6bcf3a3ad6ddbb16dbbf4a2ca7cb1
marianmagur <marianmagur@gmail.com>
2018-11-27 21:51:44 +0200

    add yakobstal num code

---
+++ Dynamic_Programming/code/yakobstelnum.py
@@ -0,0 +1,6 @@
+""""count yacobstel number"""
+def yakobstelelement (n_j):
+
+    return(2**n_j-(-1)**n_j)/3
+
+print (yakobstelelement(4))
--- yakobstelnum.py
+++
@@ -1,3 +0,0 @@
-def yakobstelelement(n):
-    return (2**n-(-1)**n)/3
-print yakobstelelement(8)
