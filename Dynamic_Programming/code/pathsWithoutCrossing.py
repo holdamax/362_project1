ebcf1979e7820ccf67feeb875df3e31781ce17f9
nazar kostiv <mr.nkostiv@gmail.com>
2018-11-28 07:07:55 +0200

    add tests

--- Dynamic_Programming/code/pathsWithoutCrossing.py
+++ Dynamic_Programming/code/pathsWithoutCrossing.py
@@ -1,14 +1,24 @@
+#!/usr/bin/env python
+"""Tests for friend_pairs.py"""
+
 from math import factorial
 
 
-def catalanNumbrs(n):
+def pathsWithoutCrossing(num):
+    """
+    ...
+    :param num:
+    :return:
+    """
     try:
-        if not n % 2:
-            return round(factorial(n)/(factorial((n/2+1))*factorial(n/2)))
+        if num % 2 or num < 2:
+            raise ValueError
+        else:
+            return round(factorial(num) / (factorial((num / 2 + 1)) * factorial(num / 2)))
     except (TypeError, ValueError):
         print('You used wrong value, please use positive, even and integer value.\n')
 
 
 if __name__ == '__main__':
-    catalan = int(input('Please enter positive, even and integer value... '))
-    print(catalanNumbrs(catalan))
+    Catalan = int(input('Please enter positive, even and integer value... '))
+    print(pathsWithoutCrossing(Catalan))
