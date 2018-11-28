03f65589e5a05b827388fe963ce1c09ce08c592b
Alexander <Sanyarozl@gmail.com>
2018-11-28 20:27:52 +0200

    Improved tasks

--- Dynamic_Programming/code/fibonacci_mod.py
+++ Dynamic_Programming/code/fibonacci_mod.py
@@ -4,8 +4,19 @@ Find which is the member of position n
 """
 
 
+def descr():
+    size = input("Please input position of fibonacci number: ")
+    try:
+        return fibo(int(size))
+    except:
+        print("Wrong inputs. Input one positive integer number.")
+
+
 def fibo(size):
     """Calculate Modified Fibonacci number."""
+
+    if size < 1:
+        raise TypeError
     if size in (1, 2, 3):
         return 1
     return fibo(size-1) + fibo(size-3)
--- Dynamic_Programming/code/min_max_afford.py
+++ Dynamic_Programming/code/min_max_afford.py
@@ -3,9 +3,26 @@ Work to be with High-effort or with Low-effort.
 """
 
 
+def descr():
+    try:
+        print("Please input number of days and two lists of affords: ")
+        days_in = input("Number of days: ")
+        low_in = []
+        high_in = []
+        for i in range(int(days_in)):
+            low_in.append(int(input("Low afford " + str(i + 1) + ": ")))
+            high_in.append(int(input("High afford " + str(i + 1) + ": ")))
+        return max_afford(low_in, high_in, int(days_in))
+    except:
+        print("Wrong inputs. Input only positive integer numbers.")
+
+
 def max_afford(low, high, days):
     """Calculate Max afford with given value of work by days."""
-    if days <= 0:
-        return 0
-    return max(high[days-1] + max_afford(low, high, (days-2)),
-               low[days-1] + max_afford(low, high, (days-1)))
+    try:
+        if days <= 0:
+            return 0
+        return max(high[days-1] + max_afford(low, high, (days-2)),
+                   low[days-1] + max_afford(low, high, (days-1)))
+    except TypeError:
+        print("Wrong inputs")
