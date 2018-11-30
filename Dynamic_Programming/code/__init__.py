f6b4c9ff5783ff736565bb22e45e45c1530d7200
Serge [Tempos] Kutwicki <Serge.Kutwicki@gmail.com>
2018-11-30 23:38:02 +0200

    Update __init__.py

--- Dynamic_Programming/code/__init__.py
+++ Dynamic_Programming/code/__init__.py
@@ -1,7 +1,9 @@
-__all__ = ['fibonacci_mod',
+__all__ = ['fibinacci',
+           'fibonacci_mod',
            'ways_to_sum',
            'min_max_afford',
            'longest_sequence',
            'ways_tile_floor',
            'paths_without_crossing',
+           'friend_pairs',
            'optimized_painting_fence']
