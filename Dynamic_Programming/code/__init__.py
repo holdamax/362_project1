77fb306a26d5131c61dd3e4f7a96f48b64c5b3a1
RomanDyda <zvirdvd@gmail.com>
2018-12-01 13:48:19 +0200

    Add ways to cover in 3 steps

--- Dynamic_Programming/code/__init__.py
+++ Dynamic_Programming/code/__init__.py
@@ -1,2 +1,2 @@
 
-__all__ = ['fibonacci', 'fibonacci_mod', 'ways_to_sum', 'min_max_afford', 'ways_tile_floor', 'paths_without_crossing', 'optimized_painting_fence']
+__all__ = ['ways_to_cover_in_3_steps', 'fibonacci', 'fibonacci_mod', 'ways_to_sum', 'min_max_afford', 'ways_tile_floor', 'paths_without_crossing', 'optimized_painting_fence']
--- project.py
+++ project.py
@@ -3,6 +3,7 @@ from Dynamic_Programming.code import *
 TASKS = {'1': fibonacci,
          '2': fibonacci_mod,
          '5': ways_to_sum,
+         '7': ways_to_cover_in_3_steps,
          '8': paths_without_crossing,
          '9': min_max_afford,
          '14': optimized_painting_fence}
