b624f8665627f9d557264272e1d26bf1da95bc50
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-02 21:46:20 +0200

    Update palindrom.py

--- Dynamic_Programming/code/palindrom.py
+++ Dynamic_Programming/code/palindrom.py
@@ -1,38 +1,48 @@
 """We find the longest such subsequence using dynamic programming
 over intervals within the string. We define the value Opt[i,j]
  for each interval [i,j] as the length of the longest palindromic subsequence"""
+
+
 def longest_pal(str):
+    """calculate longest palindrom"""
     n = len(str)
+    if n > 100:
+        return "String not more than 100 symbols !"
+    else:
+        # initialize opt Table
+        opt = [[0 for i in range(n)] for j in range(n)]
 
-    # initialize Opt Table
-    Opt = [[0 for i in range(n)] for j in range(n)]
+        # opt of single char is 1
+        for i in range(n):
+            opt[i][i] = 1
 
-    # Opt of single char is 1
-    for i in range(n):
-        Opt[i][i] = 1
-
-    # Opt for adjacent chars is 2 if same, 1 otherwise
-    for i in range(n - 1):
-        if str[i] == str[i + 1]:
-            Opt[i][i + 1] = 2
-        else:
-            Opt[i][i + 1] = 1
-
-        # we now define sil as (s)substring (i)interval (l) length of the
-        # interval [i,j] --- sil=(j-i +1) and j = i+sil-1
-
-        # we compute Opt table entry for each sil length and
-        # starting index i
-
-
-    for sil in range(2, n + 1):
-        for i in range(n - sil + 1):
-            j = i + sil - 1
-            if (str[i] == str[j]):
-                Opt[i][j] = Opt[i + 1][j - 1] + 2;
+        # opt for adjacent chars is 2 if same, 1 otherwise
+        for i in range(n - 1):
+            if str[i] == str[i + 1]:
+                opt[i][i + 1] = 2
             else:
-                Opt[i][j] = max(Opt[i][j - 1], Opt[i + 1][j])
+                opt[i][i + 1] = 1
 
-    return Opt[0][n - 1]
+            # we now define sil as (s)substring (i)interval (l) length of the
+            # interval [i,j] --- sil=(j-i +1) and j = i+sil-1
+            # we compute opt table entry for each sil length and
+            # starting index i
 
-print(longest_pal('banana'))
+        for sil in range(2, n + 1):
+            for i in range(n - sil + 1):
+                j = i + sil - 1
+                if str[i] == str[j]:
+                    opt[i][j] = opt[i + 1][j - 1] + 2
+                else:
+                    opt[i][j] = max(opt[i][j - 1], opt[i + 1][j])
+
+        print(opt[0][n - 1])
+
+
+def descr():
+    while True:
+        str = input("Please type something here and we will try to find the longest palindrom:")
+        if str == 'q':
+            print("That's it.")
+            return 'q'
+        longest_pal(str)
