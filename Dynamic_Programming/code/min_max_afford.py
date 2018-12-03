98b7a364eeb4a8a8b60d5568005d86c0f1ed593c
Alexander <Sanyarozl@gmail.com>
2018-12-03 21:00:50 +0200

    Add tests

--- Dynamic_Programming/code/min_max_afford.py
+++ Dynamic_Programming/code/min_max_afford.py
@@ -4,6 +4,7 @@ Work to be with High-effort or with Low-effort.
 
 
 def descr():
+    """Description function for checking inputs"""
     try:
         print("Please input number of days and two lists of affords: ")
         days_in = input("Number of days: ")
@@ -12,16 +13,15 @@ def descr():
         high_in = input("High afford: ")
         high_in = [int(i) for i in high_in.split(",")]
         return max_afford(low_in, high_in, int(days_in))
-    except:
+    except (TypeError, ValueError):
         print("Wrong inputs. Input only positive integer numbers.")
+    except IndexError:
+        print("Wrong inputs. Number of affords should be equal to number of days.")
 
 
 def max_afford(low, high, days):
     """Calculate Max afford with given value of work by days."""
-    try:
-        if days <= 0:
-            return 0
-        return max(high[days-1] + max_afford(low, high, (days-2)),
-                   low[days-1] + max_afford(low, high, (days-1)))
-    except TypeError:
-        print("Wrong inputs")
+    if days <= 0:
+        return 0
+    return max(high[days-1] + max_afford(low, high, (days-2)),
+               low[days-1] + max_afford(low, high, (days-1)))
