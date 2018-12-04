09f284704b3e1b2eeefd9d33d12838db17c54227
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-04 12:12:06 +0200

    Update yakobstelnum.py

--- Dynamic_Programming/code/yakobstelnum.py
+++ Dynamic_Programming/code/yakobstelnum.py
@@ -1,13 +1,15 @@
-""""count yacobstel number"""
+""""countwhich is the member of position num
+         in Yacobstel sequence"""
 
 
 def yakobstelelement(num):
     """calculate sequence"""
     if num.isdigit():
         num = int(num)
-        print(int((2**num-(-1)**num)/3))
+        return int((2**num-(-1)**num)/3)
+
     else:
-        print('Error. The values entered must be integer')
+        return 'Error. The values entered must be integer'
 
 
 def descr():
