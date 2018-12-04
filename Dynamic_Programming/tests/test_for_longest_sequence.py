0ca0abb216fd2c023758daaad1563e3a48651a6a
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-04 20:54:03 +0200

    Update test_for_longest_sequence.py

--- Dynamic_Programming/tests/test_for_longest_sequence.py
+++ Dynamic_Programming/tests/test_for_longest_sequence.py
@@ -11,9 +11,9 @@ from Dynamic_Programming.code import longest_sequence
                           ('1,2,3,4,5,6,7,8', 8),
                           ('3, 4, 7, 8, 9', 3),
                           ])
-def test_itr(inputs, outputs):
+def test_longest_seq_with_diff_one(inputs, outputs):
     """Tests for positive results"""
-    assert longest_sequence.itr(inputs) == outputs
+    assert longest_sequence.longest_seq_with_diff_one(inputs) == outputs
 
 
 @pytest.mark.parametrize('inputs, outputs',
@@ -21,6 +21,6 @@ def test_itr(inputs, outputs):
                           ('Hi', None),
                           ('', None),
                           (' ', None)])
-def test_itr(inputs, outputs):
+def test_longest_seq_with_diff_one(inputs, outputs):
     """Tests for negative results"""
-    assert longest_sequence.itr(inputs) == outputs
+    assert longest_sequence.longest_seq_with_diff_one(inputs) == outputs
