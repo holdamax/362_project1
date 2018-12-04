a3e52f10e2d6ea524e927053e81aaec564af994f
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-04 16:15:29 +0200

    Update yakobstelnum.py

--- Dynamic_Programming/code/yakobstelnum.py
+++ Dynamic_Programming/code/yakobstelnum.py
@@ -6,19 +6,32 @@ def yakobstelelement(num):
     """calculate sequence"""
     if num.isdigit():
         num = int(num)
-        print(int((2**num-(-1)**num)/3))
-        return int((2**num-(-1)**num)/3)
+        if num < 1000:
+            print(int((2**num-(-1)**num)/3))
+            return int((2**num-(-1)**num)/3)
+        else:
+            print('Error. The values entered is to large.')
+            return 'Error. The values entered is to large.'
+
+    elif num[0] == " ":
+        print('Error. The values entered must be integer.')
+        return 'Error. The values entered must be integer.'
 
     else:
-        print('Error. The values entered must be integer')         
-        return 'Error. The values entered must be integer'
+        print('Error. The values entered must be positive integer.')
+        return 'Error. The values entered must be positive integer.'
 
 
 def descr():
     while True:
         num = input("Please input yacobstel number:")
         if num == 'q':
-            print('You have finished working with the interesting row function')
+            print('You have finished working with the interesting row function.')
             return 'q'
 
         yakobstelelement(num)
+
+
+if __name__ == '__main__':
+
+    descr()
