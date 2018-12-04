cf2c8f92961cd69b758e74acdcb214e4e36dfa13
RomanDyda <zvirdvd@gmail.com>
2018-12-04 14:01:05 +0200

    -

--- Dynamic_Programming/tests/test_fibonacci_mod1.py
+++ Dynamic_Programming/tests/test_fibonacci_mod1.py
@@ -3,44 +3,27 @@ import pytest
 import Dynamic_Programming.code.fibonacci_mod as f_mod
 
 
-def test_descr_output(capsys):
-    """Test for function descr when input is 'q'."""
-    f_mod.input = lambda x: 'q'
-    f_mod.descr()
-    captured = capsys.readouterr()
-    assert captured.out == 'You have finished working with the Fibonacci mod function\n'
+# @pytest.mark.xfail(raises=(TypeError, ValueError, RecursionError))
+# @pytest.mark.parametrize("inputs", [(4, 2, 3, 4), [0, 2, 1, 6, 1], {1: 2, 3: 4}, 'jffkflоарблід', -1, 0, 4.5, ' ', ''])
+# def test_fibo_wrong_value_raise(inputs):
+#     """Test on wrong input type."""
+#     f_mod.fibo(inputs)
 
-
-def test_fibo_output6(capsys):
-    """Test for function fibo(6)."""
-    f_mod.fibo(6)
-    captured = capsys.readouterr()
-    assert captured.out == '4\n'
-#
-#
-# def test_fibo_output1(capsys):
-#     """Test for function fibo(1)."""
-#     f_mod.fibo(1)
-#     captured = capsys.readouterr()
-#     assert captured.out == '1\n'
-
-
-@pytest.mark.xfail(raises=(TypeError, ValueError, RecursionError))
-@pytest.mark.parametrize("inputs", [(4, 2, 3, 4), [0, 2, 1, 6, 1], {1: 2, 3: 4}, 'jffkflоарблід', -1, 0, 4.5, ' ', ''])
-def test_fibo_wrong_value_raise(inputs):
+@pytest.mark.xfail(raises=(TypeError))
+def test_fibo_wrong_value_raise():
     """Test on wrong input type."""
-    f_mod.fibo(inputs)
+    f_mod.fibo([0, 2, 1, 6, 1]) != None
 
-
-def test_fibo_one():
+def test_fibo_o():
     """Test of calculating fibonacci number when input is 1."""
-    assert f_mod.fibo(1) == 1
+    assert f_mod.fibo(-1) == None
+
 
 def test_fibo_null():
     """Test of calculating fibonacci number when input is 0."""
     assert f_mod.fibo(0) == None
 
-@pytest.mark.parametrize("inputs, outputs", [(1, 1), (2, 1), (3, 1), (4, 2), (5, 3), (6, 4), (7, 6), (8, 9), (17, 277)])
+@pytest.mark.parametrize("inputs, outputs", [(1, 1), (3, 1), (4, 2), (6, 4), (17, 277)])
 def test_fibo_positive(inputs, outputs):
     """Test of calculating fibonacci number with different inputs."""
     assert f_mod.fibo(inputs) == outputs
