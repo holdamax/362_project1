872fee5536a859bd196aa5d5270c85fed30c3cce
Tempos <serge.kutwicki@gmail.com>
2018-12-05 01:03:00 +0200

    Rewrote the code to correctly handle inputs.

--- Dynamic_Programming/code/longest_sequence.py
+++ Dynamic_Programming/code/longest_sequence.py
@@ -13,7 +13,7 @@ def descr():
     """Entry point function for the menu app."""
     cls()
     while True:
-        in_list = input('\nPlease, input the list of positive integers, '
+        in_list = input('\nPlease, input sequence of coma separated integers, '
                         'which has to be checked (e.g. 3, 4, 7, 8, 9): ')
         if in_list == 'q':
             print('Bye')
@@ -24,39 +24,50 @@ def descr():
             longest_seq_with_diff_one(in_list)
 
 
-def longest_seq_with_diff_one(arr):
+def count_it(arr):
+    arr = sorted(arr)
+    print(f"Using list {arr}...")
+
+    num = len(arr)
+    arr_1 = [1 for _ in range(len(arr))]
+
+    for i in range(num):
+        for j in range(i):
+            if (arr[i] == arr[j] + 1) or (arr[i] == arr[j] - 1):
+                arr_1[i] = max(arr_1[i], arr_1[j] + 1)
+
+    print(f"Longest sequence of numbers with difference one is: {max(arr_1)}")
+    return max(arr_1)
+
+
+def longest_seq_with_diff_one(arr: int or str, *args: int or str) -> int or str:
     """Find how many numbers we have with difference one in the sub sequence.
-<<<<<<< HEAD
-    Input: list of strings - Sequence with integer numbers
-=======
 
-    Input: list of strings - Sequence with integer numbers
+    Input: coma separated integers - Sequence with integer numbers
 
->>>>>>> origin
-    Returns
-    -------
-    Number, which represents the longest sequence of integers with difference one.
+    Returns: Number, which represents the longest sequence of integers with difference one.
     """
-    try:
-        if arr == '':
-            arr = [3, 4, 7, 8, 9]
-            print(f"Using default list {arr}...")
-        else:
-            arr = sorted([int(i) for i in arr.split(',')])
-            print(f"Using list {arr}...")
-
-        num = len(arr)
-        arr_1 = [1 for _ in range(len(arr))]
-
-        for i in range(num):
-            for j in range(i):
-                if (arr[i] == arr[j] + 1) or (arr[i] == arr[j] - 1):
-                    arr_1[i] = max(arr_1[i], arr_1[j] + 1)
-
-        print(f"Longest sequence of numbers with difference one is: {max(arr_1)}")
-        return max(arr_1)
-    except ValueError:
-        print('Please enter correct input.')
+    if arr == '':
+        print("Please enter correct input.")
+        return "Please enter correct input."
+    elif isinstance(arr, int):
+        lst = []
+        lst.append(arr)
+        for arg in args:
+            if str(arg).isdigit():
+                lst.append(arg)
+        count_it(lst)
+    elif isinstance(arr, list):
+        for arg in args:
+            arr.append(arg)
+            print('here')
+    elif isinstance(arr, str):
+        try:
+            arr = [int(i) for i in arr.split(',')]
+            count_it(arr)
+        except ValueError:
+            print("Please enter correct input.")
+            return "Please enter correct input."
 
 
 if __name__ == '__main__':
