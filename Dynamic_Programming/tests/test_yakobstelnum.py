147860e35bc562bbc62290a89f990bf464646449
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-04 11:30:41 +0200

    Update test_yakobstelnum.py

--- Dynamic_Programming/tests/test_yakobstelnum.py
+++ Dynamic_Programming/tests/test_yakobstelnum.py
@@ -20,6 +20,7 @@ def test_fail():
     yakobstelelement('qwerty')
 
 
-@pytest.mark.parametrize("yakobstelel_num, yakobstel_val", [(1, 1), (2, 1), (5, 11), (7, 43), (9, 171)])
-def test_yakob(yakobstelel_num, yakobstel_val):
+@pytest.mark.parametrize("inputs, outputs", [(1, 1), (2, 1), (5, 11), (7, 43), (9, 171)])
+def test_yakob(inputs, outputs):
     """calculate yakobstel number with different inputs."""
+    assert yakobstelelement(inputs) == outputs
