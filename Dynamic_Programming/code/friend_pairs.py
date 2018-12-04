c9c37bb1eba18aed489befcd134446d46ef404e1
Tempos <serge.kutwicki@gmail.com>
2018-12-04 23:07:55 +0200

    Added input check for ints and floats.

--- Dynamic_Programming/code/friend_pairs.py
+++ Dynamic_Programming/code/friend_pairs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Friend pairs.
-
 For a group of n friends, let f(n) be the number of ways how they can be paired up or remain single.
 Then either the n-th person remains single, or paired up.
 If the n-th person is single, then we recur for (n - 1) friends.
@@ -20,36 +19,29 @@ def descr():
     while True:
         choice = input(
             '\n>>> Please enter integer positive number of friends or X, if you want to exit: ')
+        choice = choice.lower()
+        if choice == 'q':
+            print('Bye\n')
+            return choice
+        if choice == 'c':
+            cls()
+        else:
+            itr(choice)
 
-        itr(choice)
 
-
-def itr(num_of_friends: str) -> (int or str):
+def itr(num_of_friends: str) -> int:
     """Count all possible pairings.
-
     Function takes in number of friends and outputs the total number of ways in which friends
     can remain single or can be paired up.
-
     Parameters
     ----------
-    num_of_friends: integer positive number
-
+    num_of_friends : str     number of friends
     Returns
     -------
     f(n) = f(n-1) + (n-1) * f(n-2)
-    total number of ways in which friends can remain single or can be paired up."""
-
-# Inputs / Exit / Clear
-    num_of_friends: str = num_of_friends.lower()
-    if num_of_friends == 'q':
-        print('Bye\n')
-        cls()
-        return num_of_friends
-    if num_of_friends == 'c':
-        cls()
-
-# Looking for result here
-    if num_of_friends.isdigit():
+        total number of ways in which friends can remain single or can be paired up.
+    """
+    if str(num_of_friends).isdigit():
         num_of_friends = int(num_of_friends)
         if num_of_friends == 0:
             result = 0
@@ -61,7 +53,7 @@ def itr(num_of_friends: str) -> (int or str):
             for i in range(2, num_of_friends + 1):
                 result = f_1 + (i - 1) * f_2
                 f_2, f_1 = f_1, result
-        print(f"Number of ways to pair {num_of_friends} friends is:\t {result}")
+        print(f"Number of ways to pair {num_of_friends} friends is: {result}")
         return result
     else:
         print("You gave wrong input. Try again.")
