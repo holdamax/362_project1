697855113221541536002432aa7dbbbcffbb46fe
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-06 00:24:45 +0200

    Update countingsort.py

--- Sorting/code/countingsort.py
+++ Sorting/code/countingsort.py
@@ -3,15 +3,17 @@
 
 
 def counting_sort(data):
-    dat = data.split()
-    max_number = (max(dat))
-    min_number = (min(dat))
+
+    member_list=[]
+    for i in data.split(','):
+        member_list.append(int(i))
+    max_number = (max(member_list))
+    min_number = (min(member_list))
     n = max_number - min_number + 1
     # list for counters
     counters = [0] * n
-    for v in dat:
+    for v in member_list:
         counters[v - min_number] += 1
-        #print(counters)
     result = []
     count = -1
     for i in counters:
@@ -19,11 +21,11 @@ def counting_sort(data):
         result.extend([count + min_number] * i)
     print(result)
     return result
-counting_sort('1,2,3,7')
+
 def descr():
     """Entry point function for the menu app."""
     while True:
-        data = input("Please type list of integers in format [1,37,3,8] and i sort it:")
+        data = input("Please type list of integers and i sort it:")
         if data == 'q':
             print("That's it.")
             return 'q'
