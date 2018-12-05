627d9f87d45d7d10755aa820fee7253679ddcc07
Alexander <Sanyarozl@gmail.com>
2018-12-05 23:28:24 +0200

    Improved Tests

--- Dynamic_Programming/tests/test_pytest_palindrom.py
+++ Dynamic_Programming/tests/test_pytest_palindrom.py
@@ -6,18 +6,21 @@ from Dynamic_Programming.code.palindrom import longest_pal
                                 
 def test_func():
     """Test with given input"""
-    assert longest_pal('Test sequence ololo') == 5
+    assert longest_pal("Test sequence qwqwq") == 5
 
 
-def test_error():
-    """Test of raising exception."""
-    pytest.raises(TypeError, "longest_pal(10)")
+@pytest.mark.parametrize("input, exception", [("", IndexError), (10, TypeError)])
+def test_errors(input, exception):
+    """Test of raising exceptions."""
+    with pytest.raises(exception):
+        longest_pal(input)
 
 
-@pytest.mark.parametrize("string, palindrom_length", [("no palindrom", 1),
-                                                      ("testset", 7),
-                                                      ("lol  kekek", 5),
-                                                      ("Tesing longestpalaptsegnol algoritm 12321", 21)])
-def test_fibo(string, palindrom_length):
+@pytest.mark.parametrize("input, palindrom_length", [("qwerty qwerty", 1),
+                                                    ("testset", 7),
+                                                    ("lol  kekek", 5),
+                                                    ("Tesing longestpalaptsegnol algoritm 12321", 21)
+                                                    ])
+def test_fibo(input, palindrom_length):
     """Test with different inputs."""
-    assert longest_pal(string) == palindrom_length
+    assert longest_pal(input) == palindrom_length
