3dd3e54ceb548e16b081e20ced12e9b7455db895
marianmagur <33460464+marianmagur@users.noreply.github.com>
2018-12-05 21:01:50 +0200

    Update palindrom.py

--- Dynamic_Programming/code/palindrom.py
+++ Dynamic_Programming/code/palindrom.py
@@ -1,32 +1,52 @@
 """We find the longest such subsequence using dynamic programming
-over intervals within the string. We define the length of the longest
- palindromic subsequence"""
+over intervals within the string. We define the value Opt[i,j]
+ for each interval [i,j] as the length of the longest palindromic subsequence"""
 
 
-def longest_pal(inp):
-    """calculating longest palindrom"""
-    inp_str = str(inp)
-    if len(inp_str) >= 100:
-        return 'String should not be longer than 100 characters.'
-    if len(inp_str) == 0 or inp_str == '':
-        return str(len(inp_str)) + "\n" + inp_str
-    if inp_str == inp_str[::-1]:
-        return str(len(inp_str)) + "\n" + inp_str
-    for i in range(len(inp_str)-1, 0, -1):
-        for j in range(len(inp_str)-i+1):
-            temp = inp_str[j:j+i]
-            if temp == temp[::-1]:
-                print(str(len(temp)) + "\n" + temp)
-                return str(len(temp)) + "\n" + temp
+def longest_pal(inp_str):
+    """calculate longest palindrom"""
+    len_inp_str = len(inp_str)
+    if len_inp_str > 100:
+        return "String not more than 100 symbols !"
+
+    # initialize opt Table
+    opt = [[0 for i in range(len_inp_str)] for j in range(len_inp_str)]
+
+    # opt of single char is 1
+    for i in range(len_inp_str):
+        opt[i][i] = 1
+
+    # opt for adjacent chars is 2 if same, 1 otherwise
+    for i in range(len_inp_str - 1):
+        if inp_str[i] == inp_str[i + 1]:
+            opt[i][i + 1] = 2
+        else:
+            opt[i][i + 1] = 1
+
+        # we now define sil as (s)substring (i)interval (l) length of the
+        # interval [i,j] --- sil=(j-i +1) and j = i+sil-1
+        # we compute opt table entry for each sil length and
+        # starting index i
+
+    for sil in range(2, len_inp_str + 1):
+        for i in range(len_inp_str - sil + 1):
+            j = i + sil - 1
+            if inp_str[i] == inp_str[j]:
+                opt[i][j] = opt[i + 1][j - 1] + 2
+            else:
+                opt[i][j] = max(opt[i][j - 1], opt[i + 1][j])
+    print(opt[0][len_inp_str - 1])
+    return opt[0][len_inp_str - 1]
 
 
 def descr():
     while True:
-        inp = input("Please type something here and we will try to find the longest palindrom:")
-        if inp == 'q':
+        inp_str = input("Please type something here and we will try to find the longest palindrom:")
+        if inp_str == 'q':
             print("That's it.")
             return 'q'
-        longest_pal(inp)
+
+        longest_pal(inp_str)
 
 
 if __name__ == '__main__':
