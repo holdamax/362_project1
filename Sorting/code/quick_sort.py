faf1b04d1324b2ccb540dc9c25587919e05bc222
holdamax <holda.max@gmail.com>
2018-12-05 15:53:48 +0200

    update quicksort

--- Sorting/code/quick_sort.py
+++ Sorting/code/quick_sort.py
@@ -1,26 +1,39 @@
 """Quick sorting with python """
 
 
-def quickSort(alist):
-   quickSortHelper(alist,0,len(alist)-1)
+def descr():
+    """Function for to input and verification data"""
+    alist = input('Please enter list of numbers to sort like: 5, 4, 3, 2, 1 \n')
+    alist = list(map(int, alist.split(',')))
+    if alist == 'q':
+        return 'q'
+    try:
+        quicksort(alist)
+        print("Sorted list looks like: {}".format(alist))
+    except (TypeError, ValueError, IndexError):
+        print('Wrong input. Please enter list of numbers like: 5, 4, 3, 2, 1 ')
+    return None
 
-def quickSortHelper(alist,first,last):
-   if first<last:
 
-       splitpoint = partition(alist,first,last)
+def quicksort(alist: list):
+    quicksort_helper(alist,0,len(alist)-1)
 
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
+
+def quicksort_helper(alist,first,last):
+    if first<last:
+        splitpoint = partition(alist,first,last)
+        quicksort_helper(alist,first,splitpoint-1)
+        quicksort_helper(alist,splitpoint+1,last)
 
 
 def partition(alist,first,last):
-   pivotvalue = alist[first]
+    pivotvalue = alist[first]
 
-   leftmark = first+1
-   rightmark = last
+    leftmark = first+1
+    rightmark = last
 
-   done = False
-   while not done:
+    done = False
+    while not done:
 
        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
            leftmark = leftmark + 1
@@ -35,9 +48,7 @@ def partition(alist,first,last):
            alist[leftmark] = alist[rightmark]
            alist[rightmark] = temp
 
-   temp = alist[first]
-   alist[first] = alist[rightmark]
-   alist[rightmark] = temp
-
-
-   return rightmark
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+    return rightmark
