470a35808e6b78684edcef7a0301de2eedc25e49
Alexander <Sanyarozl@gmail.com>
2018-12-06 10:22:07 +0200

    Improved Tests

--- Dynamic_Programming/tests/test_pytest_palindrom.py
+++ Dynamic_Programming/tests/test_pytest_palindrom.py
@@ -6,21 +6,22 @@ from Dynamic_Programming.code.palindrom import longest_pal
                                 
 def test_func():
     """Test with given input"""
-    assert longest_pal("Test sequence qwqwq") == 5
+    assert longest_pal("Test sequence qwqwq") == "5"
 
 
-@pytest.mark.parametrize("input, exception", [("", IndexError), (10, TypeError)])
-def test_errors(input, exception):
+@pytest.mark.parametrize("inputs, ouputs", [("123456789012345678901234567890123456789012345678901"
+                                             "23456789012345678901234567890123456789012345678901",
+                                             'String should not be longer than 100 characters.')])
+def test_errors(inputs, ouputs):
     """Test of raising exceptions."""
-    with pytest.raises(exception):
-        longest_pal(input)
+    assert longest_pal(inputs) == ouputs
 
 
-@pytest.mark.parametrize("input, palindrom_length", [("qwerty qwerty", 1),
-                                                    ("testset", 7),
-                                                    ("lol  kekek", 5),
-                                                    ("Tesing longestpalaptsegnol algoritm 12321", 21)
+@pytest.mark.parametrize("inputs, palindrom_length", [("qwerty qwerty", "1"),
+                                                    ("testset", "7"),
+                                                    ("lol  kekek", "5"),
+                                                    ("Tesing longestpalaptsegnol algoritm 12321", "21")
                                                     ])
-def test_fibo(input, palindrom_length):
+def test_fibo(inputs, palindrom_length):
     """Test with different inputs."""
-    assert longest_pal(input) == palindrom_length
+    assert longest_pal(inputs) == palindrom_length
